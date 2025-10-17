add05657217f2b3ece130bb0324f5886f55e1fe0
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-10-17 18:06:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
-
 function App() {
   return (
     <div id="main">
       // Do not alter the main div
+          <ParagraphDisplay />
+
     </div>
   );
 }
-
-
 export default App;
+ function ParagraphDisplay(){
+    const[varible,paravar]=useState(false)
+    let data = (varible)? <p id="para">Hello, I've learnt to use the full-stack evaluation tool.</p> : null;
+    return(
+        <div>
+            {data}
+            <button id="click" onClick={()=>paravar(!varible)}>Click me</button>
+        </div>
+    )
+}
